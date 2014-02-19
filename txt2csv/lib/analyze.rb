09b7093063b561900f3f4c71b8a6df84ee543238
Marko Nikolovski <mz.nikolovski@gmail.com>
2014-02-19 14:08:08 -0800

    finished analyze.rb and beginning CLI implementation

--- txt2csv/lib/analyze.rb
+++ txt2csv/lib/analyze.rb
@@ -1,4 +1,4 @@
-#
+#!/usr/bin/env ruby
 class Analyze
   def initialize(input_file_name, output_file_name, fix)
     @input_file = File.new(input_file_name, 'r')
@@ -6,25 +6,31 @@ class Analyze
     @fix = fix
   end
 
-  def determine_fix
-    case @fix
+  def analyze
+    histogram = Hash.new(0)
+    pattern = pick_pattern
 
-    when '-p'
-
-
-    when '-s'  
-
-      
+    @input_file.each_line do |line| 
+      line_array = CSV.parse_line(line, :col_sep => '\t' )
+      hash_data = regex(pattern, line_array[0])
+      histogram[hash_data.to_sym] += 1
     end
-
-  def split_file
-    separate = @input_file.each_line { |line| CSV.parse_line(line, :col_sep => '\t' ) }
-  end  
-
-  def 
-
-    
+  
+    histogram = Hash[ histogram.sort_by { |word, count| count }.reverse]
+    histogram.each { |word, count| @output_file.write("#{word} #{count}") }    
+  end 
+  
+  def pick_pattern
+    case @fix
+    when '-p'
+      pattern = /^[\w\.]+/
+    when '-s'
+      pattern = /[\w]+$/
+    end
+    pattern
   end
 
-
-  end 
+  def regex(pattern, line)
+    pattern.match(line).to_s
+  end
+end
