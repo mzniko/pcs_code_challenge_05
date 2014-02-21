560647e60f3d97271ea10dfc086d53f0327a353e
Marko Nikolovski <mz.nikolovski@gmail.com>
2014-02-20 20:26:29 -0800

    finished convert.rb class and associated cli; small style changes to analyze.rb

--- txt2csv/lib/analyze.rb
+++ txt2csv/lib/analyze.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 require 'csv'
 
+# This analyzes names to create a histogram of prefixes or suffixes
 class Analyze
   def initialize(input_file_name, output_file_name, fix)
     @input_file = File.new(input_file_name, 'r')
@@ -14,13 +15,13 @@ class Analyze
     pattern = pick_pattern
 
     @input_file.each_line do |line|
-      line_array = CSV.parse_line(line, :col_sep => '\t' )
+      line_array = CSV.parse_line(line, col_sep: '\t')
       hash_data = regex(pattern, line_array[0])
       histogram[hash_data.to_sym] += 1
     end
 
     histogram = Hash[ histogram.sort_by { |word, count| count }.reverse]
-    histogram.each { |word, count| @output_file.write("#{word} #{count}\n") }
+    histogram.each { |word, count| @output_file.puts("#{word} #{count}") }
   end
 
   def pick_pattern
--- txt2csv/lib/convert.rb
+++ txt2csv/lib/convert.rb
@@ -9,29 +9,26 @@ class Convert
     @suffix_file = File.new(suffix_file_name, 'r')
     @output_file = File.new(output_file_name, 'w')
     @prefix = Array.new
-    @suffix = Array.new   
+    @suffix = Array.new
+    create_fix_arrays
+    send_to_parser
   end
 
-  def convert
+  def create_fix_arrays
+    @prefix_file.each_line { |line| @prefix << line.split[0] }
+    @suffix_file.each_line { |line| @suffix << line.split[0] }
+  end
 
-    @prefix_file.each_line do |line| 
-      @prefix << line.split[0]
+  def send_to_parser
+    @input_file.each_line do |line|
+      parsed_strings = []
+      unparsed_strings_array = CSV.parse_line(line, :col_sep => '\t')
+      parsed_strings << Parse.parse_names(@prefix, @suffix, unparsed_strings_array[0])
+      parsed_strings << Parse.parse_phone(unparsed_strings_array[1])
+      parsed_strings << Parse.parse_twitter(unparsed_strings_array[2])
+      parsed_strings << Parse.parse_email(unparsed_strings_array[3])
+      @output_file.puts(parsed_strings.join(" "))
+    end
+  end
 
-    @suffix_file.each_line do |line|
-      @suffix = line.split[0]
-    
-
-
-
-   end    
-
-
-
-  end 
-
-
-
-
-
-
-end  
+end
